c93efbbcff938984b10ac3073869bad29a02f9de
mozes32 <moussaebrahem32@gmail.com>
2023-01-24 23:13:52 +0200

    beta contact page

--- src/Shared/Components/Navbar.jsx
+++ src/Shared/Components/Navbar.jsx
@@ -1,11 +1,10 @@
 import { NavLink } from "react-router-dom";
 import navLogo from "../../assets/images/nav-logo.svg";
-import langIcon from "../../assets/images/lang-icon.svg"
+import langIcon from "../../assets/images/lang-icon.svg";
 import LangDropdown from "./LangDropdown";
 import ProjectsDropdown from "./ProjectsDropdown";
 
-
-const Navbar = ({background}) => {
+const Navbar = ({ background }) => {
   return (
     <div className="bg-slate-800">
       <div
@@ -14,16 +13,24 @@ const Navbar = ({background}) => {
       >
         <img className="my-5 md:my-0" src={navLogo} alt="navbar logo" />
         <div className="flex flex-col md:flex-row justify-between items-center md:gap-3 lg:gap-8 text-base font-medium">
-          <NavLink className="mb-3 md:mb-0" to="/">Home</NavLink>
+          <NavLink className="mb-3 md:mb-0" to="/">
+            Home
+          </NavLink>
           <div className="mb-3 md:mb-0" to="/">
-            <ProjectsDropdown/>
+            <ProjectsDropdown />
           </div>
-          <NavLink className="mb-3 md:mb-0" to="/blog">Blog</NavLink>
-          <NavLink className="mb-3 md:mb-0" to="/blog">Documentation</NavLink>
-          <NavLink className="mb-3 md:mb-0" to="/contact-us">Contact</NavLink>
+          <NavLink className="mb-3 md:mb-0" to="/blog">
+            Blog
+          </NavLink>
+          <NavLink className="mb-3 md:mb-0" to="/blog">
+            Documentation
+          </NavLink>
+          <NavLink className="mb-3 md:mb-0" to="/contact-us">
+            Contact
+          </NavLink>
           <div className="bg-white bg-opacity-20 rounded-[30px] px-4 py-2">
-            <img className="inline-block mr-2" src={langIcon} alt="language icon"/>
-            <LangDropdown/>
+            <img className="inline-block mr-2" src={langIcon} alt="language icon" />
+            <LangDropdown />
           </div>
         </div>
       </div>
