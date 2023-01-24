618142c1d45db78e16fe55235a55775be1a80e26
mozes32 <moussaebrahem32@gmail.com>
2023-01-24 15:43:32 +0200

    refactor: make description max width 762px

--- src/Shared/Static/StaticPageHeader.jsx
+++ src/Shared/Static/StaticPageHeader.jsx
@@ -8,7 +8,7 @@ const StaticPageHeader = ({ summary, title, description }) => {
         <header className="container mx-auto text-center flex justify-center items-center flex-col h-full">
           <span className="block text-indigo-500 font-medium text-md lg:text-lg mb-4 xl:mt-[45px]">{summary}</span>
           <h1 className="text-2xl md:text-3xl lg:text-5xl font-semibold">{title}</h1>
-          <p className="text-md lg:text-xl font-medium mt-8">{description}</p>
+          <p className="text-md lg:text-xl font-medium mt-8 max-w-[762px]">{description}</p>
         </header>
       </section>
     </>
