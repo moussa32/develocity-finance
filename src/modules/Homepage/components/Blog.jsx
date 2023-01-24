b74a612f18a629efdd0aef4f4ef170d1c06ac630
Malihassan <momenalhendawy@gmail.com>
2023-01-25 01:29:31 +0200

    add blog style

--- src/modules/Homepage/components/Blog.jsx
+++ src/modules/Homepage/components/Blog.jsx
@@ -7,16 +7,14 @@ function Blog() {
   return (
     <div className="text-center my-32">
       <div className="container mx-auto">
-        <h3 className="text-6xl  text-[#29233B]">
-          Develocity Blog
-        </h3>
+        <h3 className="text-6xl  text-[#29233B]">Develocity Blog</h3>
         <p className="text-sm mt-4 capitalize text-[#525C7A]">
           News, updates, and useful content from across the Develocity
           Ecosystem.
         </p>
-        <div className="w-10/12 mt-10 container mx-auto">
+        <div className=" mt-10 container mx-auto">
           <div className=" gap-44 lg:gap-9 grid grid-cols-1 lg:grid-cols-2">
-            <div className="shadow-xl flex flex-col rounded-lg">
+            <div className="shadow-xl flex flex-col rounded-lg z-10">
               <div className="text-left w-5/6 container mx-auto mt-10 flex flex-col">
                 <p className="text-3xl capitalize text-[#29233B]">
                   We are launching App for Multi-functional Tool
@@ -32,15 +30,17 @@ function Blog() {
                   Learn More →
                 </Link>
               </div>
-              <div className="relative h-72">
-                <img className="absolute" src={phoneImage} />
+              <div className="relative flex justify-center h-72">
+                <div className="absolute top-16 z-0">
+                <img className="w-[500px] lg:w-full" src={phoneImage} />
+                </div>
               </div>
             </div>
 
             <div className="shadow-xl shadow-gray-300 flex flex-col pb-10 rounded-lg">
-              <div className="realtive flex flex-col justify-center bg-gradient-to-r from-[#120734] to-[#000000] h-72">
-                <div className="absolute">
-                  <img className="w-full" src={manImage} />
+              <div className="realtive flex justify-center bg-gradient-to-r from-[#120734] to-[#000000] h-64">
+                <div className="absolute self-center">
+                  <img className="w-10/12" src={manImage} />
                 </div>
               </div>
               <div className="text-left w-5/6 container mx-auto mt-20 flex flex-col ">
@@ -61,8 +61,8 @@ function Blog() {
             </div>
           </div>
         </div>
-        <p className="mt-16 relative z-10">
-          <Link style={{ color: "#6366F1" }} to={"terms-and-conditions"}>
+        <p className="mt-16 relative z-10 text-[#6366F1] font-semibold">
+          <Link to={"terms-and-conditions"}>
             Check more news and updates →
           </Link>
         </p>
