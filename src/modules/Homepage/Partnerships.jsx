dd0edd6b7a0425eb8d0afaa732d29978a7734708
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2023-01-24 16:28:33 +0200

    edit partnership section

--- src/modules/Homepage/Partnerships.jsx
+++ src/modules/Homepage/Partnerships.jsx
@@ -16,51 +16,57 @@ const Partnerships = () => {
   const [binanceSrc, setBinanceSrc] = useState(binance);
 
   return (
-    <div className="container mx-auto text-center mt-48 mb-24">
-      <h2 className="text-[#29233B] text-4xl md:text-6xl font-medium md:font-normal">Partnerships that Last</h2>
-      <h6 className="text-[#525C7A] mt-3">
+    <div className="container mx-auto bg-[#F9FAFB] text-center py-24">
+      <h2 className="text-[#29233B] font-PolySans text-4xl md:text-6xl font-medium md:font-normal">
+        Partnerships that Last
+      </h2>
+      <h6 className="text-[#525C7A] text-lg mt-3">
         We have connected with amazing people, businesses, and organizations for
-        the past 2 years. qdhj
+        the past 2 years.
       </h6>
       <div className="flex justify-evenly mt-8 px-32 mx-auto gap-1 lg:gap-14">
         <img
-        //#0052ff
+          //#0052ff
           className="w-[65px] md:w-[180px] hover:cursor-pointer"
           src={`${coinbaseSrc}`}
           alt="coinbase icon"
-          onMouseOver = {() => setCoinbaseSrc(coinbaseColored)}
-          onMouseOut = {() => setCoinbaseSrc(coinbase)}
+          onMouseOver={() => setCoinbaseSrc(coinbaseColored)}
+          onMouseOut={() => setCoinbaseSrc(coinbase)}
         />
         <img
           className="w-[65px] md:w-[180px] hover:cursor-pointer"
           src={`${bitmexSrc}`}
           alt="bitmex icon"
-          onMouseOver = {() => setBitmexSrc(bitmexColored)}
-          onMouseOut = {() => setBitmexSrc(bitmex)}
+          onMouseOver={() => setBitmexSrc(bitmexColored)}
+          onMouseOut={() => setBitmexSrc(bitmex)}
         />
         <img
           className="w-[65px] md:w-[180px] hover:cursor-pointer"
           src={`${xtComSrc}`}
           alt="xt.com icon"
-          onMouseOver = {() => setxtComSrc(xtComColored)}
-          onMouseOut = {() => setxtComSrc(xtCom)}
+          onMouseOver={() => setxtComSrc(xtComColored)}
+          onMouseOut={() => setxtComSrc(xtCom)}
         />
         <img
           className="w-[65px] md:w-[180px] hover:cursor-pointer"
           src={`${binanceSrc}`}
           alt="binance icon"
-          onMouseOver = {() => setBinanceSrc(binanceColored)}
-          onMouseOut = {() => setBinanceSrc(binance)}
+          onMouseOver={() => setBinanceSrc(binanceColored)}
+          onMouseOut={() => setBinanceSrc(binance)}
         />
       </div>
       <p className="text-2xl md:text-4xl relative font-medium text-neutral-900 mt-16 md:mt-24 lg:px-52">
-        <img className="absolute -z-10 right-28 -top-10 md:right-[47%] lg:bottom-[170px]" src={`${quote}`} alt="quote icon"/>
+        <img
+          className="absolute -z-10 right-28 -top-10 md:right-[47%] lg:bottom-[170px]"
+          src={`${quote}`}
+          alt="quote icon"
+        />
         “We entrepreneurs are loners, vagabonds, troublemakers. Success is
         simply a matter of finding and surrounding ourselves with those
         open-minded and clever souls who can take our insanity and put it to
         good use.”
       </p>
-      <h6 className="text-lg font-semibold mt-5 md:mt-7">Brian Armstrong</h6>
+      <h6 className="text-lg font-semibold mt-5 md:mt-8">Brian Armstrong</h6>
       <h6 className="text-[#525C7A] text-base">CEO of Coinbase</h6>
     </div>
   );
