2ec66c2a736631f9a381973b41ffe7a26007681b
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2023-01-25 11:23:46 +0200

    edit partnerships background and quote icon

--- src/modules/Homepage/Partnerships.jsx
+++ src/modules/Homepage/Partnerships.jsx
@@ -16,58 +16,62 @@ const Partnerships = () => {
   const [binanceSrc, setBinanceSrc] = useState(binance);
 
   return (
-    <div className="container mx-auto bg-[#F9FAFB] text-center py-24">
-      <h2 className="text-[#29233B] font-PolySans text-4xl md:text-6xl font-medium md:font-normal">
-        Partnerships that Last
-      </h2>
-      <h6 className="text-[#525C7A] text-lg mt-3">
-        We have connected with amazing people, businesses, and organizations for
-        the past 2 years.
-      </h6>
-      <div className="flex justify-evenly mt-8 px-32 mx-auto gap-1 lg:gap-14">
-        <img
-          //#0052ff
-          className="w-[65px] md:w-[180px] hover:cursor-pointer"
-          src={`${coinbaseSrc}`}
-          alt="coinbase icon"
-          onMouseOver={() => setCoinbaseSrc(coinbaseColored)}
-          onMouseOut={() => setCoinbaseSrc(coinbase)}
-        />
-        <img
-          className="w-[65px] md:w-[180px] hover:cursor-pointer"
-          src={`${bitmexSrc}`}
-          alt="bitmex icon"
-          onMouseOver={() => setBitmexSrc(bitmexColored)}
-          onMouseOut={() => setBitmexSrc(bitmex)}
-        />
-        <img
-          className="w-[65px] md:w-[180px] hover:cursor-pointer"
-          src={`${xtComSrc}`}
-          alt="xt.com icon"
-          onMouseOver={() => setxtComSrc(xtComColored)}
-          onMouseOut={() => setxtComSrc(xtCom)}
-        />
-        <img
-          className="w-[65px] md:w-[180px] hover:cursor-pointer"
-          src={`${binanceSrc}`}
-          alt="binance icon"
-          onMouseOver={() => setBinanceSrc(binanceColored)}
-          onMouseOut={() => setBinanceSrc(binance)}
-        />
+    <div className="bg-[#F9FAFB]">
+      <div className="container mx-auto text-center py-24">
+        <h2 className="text-[#29233B] font-PolySans text-4xl md:text-6xl font-medium md:font-normal">
+          Partnerships that Last
+        </h2>
+        <h6 className="text-[#525C7A] text-lg mt-3">
+          We have connected with amazing people, businesses, and organizations
+          for the past 2 years.
+        </h6>
+        <div className="flex justify-evenly mt-8 px-32 mx-auto gap-1 lg:gap-14">
+          <img
+            //#0052ff
+            className="w-[65px] md:w-[180px] hover:cursor-pointer"
+            src={`${coinbaseSrc}`}
+            alt="coinbase icon"
+            onMouseOver={() => setCoinbaseSrc(coinbaseColored)}
+            onMouseOut={() => setCoinbaseSrc(coinbase)}
+          />
+          <img
+            className="w-[65px] md:w-[180px] hover:cursor-pointer"
+            src={`${bitmexSrc}`}
+            alt="bitmex icon"
+            onMouseOver={() => setBitmexSrc(bitmexColored)}
+            onMouseOut={() => setBitmexSrc(bitmex)}
+          />
+          <img
+            className="w-[65px] md:w-[180px] hover:cursor-pointer"
+            src={`${xtComSrc}`}
+            alt="xt.com icon"
+            onMouseOver={() => setxtComSrc(xtComColored)}
+            onMouseOut={() => setxtComSrc(xtCom)}
+          />
+          <img
+            className="w-[65px] md:w-[180px] hover:cursor-pointer"
+            src={`${binanceSrc}`}
+            alt="binance icon"
+            onMouseOver={() => setBinanceSrc(binanceColored)}
+            onMouseOut={() => setBinanceSrc(binance)}
+          />
+        </div>
+        <div className="relative">
+          <img
+              className="absolute z-10 right-28 -top-10 md:right-[47%] lg:bottom-[170px]"
+              src={`${quote}`}
+              alt="quote icon"
+            />
+          <p className="text-2xl relative z-20 md:text-4xl font-medium text-neutral-900 mt-16 md:mt-24 lg:px-52">
+            “We entrepreneurs are loners, vagabonds, troublemakers. Success is
+            simply a matter of finding and surrounding ourselves with those
+            open-minded and clever souls who can take our insanity and put it to
+            good use.”
+          </p>
+        </div>   
+        <h6 className="text-lg font-semibold mt-5 md:mt-8">Brian Armstrong</h6>
+        <h6 className="text-[#525C7A] text-base">CEO of Coinbase</h6>
       </div>
-      <p className="text-2xl md:text-4xl relative font-medium text-neutral-900 mt-16 md:mt-24 lg:px-52">
-        <img
-          className="absolute -z-10 right-28 -top-10 md:right-[47%] lg:bottom-[170px]"
-          src={`${quote}`}
-          alt="quote icon"
-        />
-        “We entrepreneurs are loners, vagabonds, troublemakers. Success is
-        simply a matter of finding and surrounding ourselves with those
-        open-minded and clever souls who can take our insanity and put it to
-        good use.”
-      </p>
-      <h6 className="text-lg font-semibold mt-5 md:mt-8">Brian Armstrong</h6>
-      <h6 className="text-[#525C7A] text-base">CEO of Coinbase</h6>
     </div>
   );
 };
