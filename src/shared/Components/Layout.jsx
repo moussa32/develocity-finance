b2e081487d297eed82f1824a6c7d8363cd9cc5af
mozes32 <moussaebrahem32@gmail.com>
2023-01-29 16:41:19 +0200

    feat: added handle navbar transparent on scroll

--- src/shared/Components/Layout.jsx
+++ src/shared/Components/Layout.jsx
@@ -1,11 +1,39 @@
-import { Outlet } from "react-router-dom";
+import { useCallback, useMemo, useState, useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 const Layout = () => {
+  const [isNavbarTransparent, setIsNavbarTransparent] = useState(true);
+  const currentRoute = useLocation();
+
+  const handleNavbarMove = useCallback(() => {
+    if (window.scrollY > 150) {
+      setIsNavbarTransparent(false);
+    } else {
+      setIsNavbarTransparent(true);
+    }
+  }, []);
+
+  const handleNavbarBackground = useMemo(() => {
+    if (isNavbarTransparent) {
+      if (currentRoute.pathname === "/") return "bg-transparent top-[40px]";
+      return "bg-white/[0.1] top-[40px]";
+    }
+
+    return "bg-indigo-500/75 top-[20px]";
+  }, [currentRoute, isNavbarTransparent]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleNavbarMove);
+    return () => window.removeEventListener("scroll", handleNavbarMove);
+  }, []);
+
   return (
     <>
-      <Navbar containerClassName="fixed bg-white/[0.1] mx-auto inset-x-0 top-[40px] z-50" />
+      <Navbar
+        containerClassName={`fixed transition ease-in-out duration-700 ${handleNavbarBackground} mx-auto inset-x-0 z-50`}
+      />
       <Outlet />
       <Footer />
     </>
