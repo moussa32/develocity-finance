0fa29a8d06b4ff9f0575006ab56865b8277f73e3
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2023-01-30 17:33:59 +0200

    edit dropdown design

--- src/shared/Components/ProjectsDropdown.jsx
+++ src/shared/Components/ProjectsDropdown.jsx
@@ -29,7 +29,7 @@ const ProjectsDropdown = () => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute mx-auto w-[90vw] p-4 inset-x-0 mt-5 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10 md:pb-8 md:px-9 lg:left-[-300px] lg:top-[25px] md:w-[680px]">
+        <Menu.Items className="absolute mx-auto h-[auto] w-[90vw] p-2 md:p-4 inset-x-0 mt-5 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10 md:pb-8 md:px-9 lg:left-[-300px] lg:top-[25px] md:w-[680px] md:h-[365px]">
           <img
             className="absolute inset-x-0 mx-auto -top-[7px] origin-top-right"
             src={dropdownArrow}
@@ -37,31 +37,33 @@ const ProjectsDropdown = () => {
           />
           <div className="py-1">
             <Menu.Item>
-              <h2 className="block w-full mt-0.5 md:mt-11 px-4 pb-2 text-sm font-semibold text-indigo-500">
+              <h2 className="block w-full mt-0.5 md:mt-5 px-4 text-sm font-semibold text-indigo-500">
                 Projects
               </h2>
             </Menu.Item>
             <div className="grid text-center md:pb-0 md:text-left grid-cols-2">
               {/*right menu items */}
               <div>
-                <span className="text-[rgb(181,71,8)] bg-[rgb(255,250,235)] px-2 ml-0 text-[10px] font-medium float-right rounded">
-                  Beta
-                </span>
                 <Menu.Item>
-                  <div className="md:flex gap-4 px-2 md:px-4 py-2 text-sm clear-both">
-                    <img
-                      className="inline-block w-12 mx-auto md:mx-0"
-                      src={multiFunctional}
-                      alt="multi functional icon"
-                    />
-                    <div className="md:mt-3">
-                      <h3 className="md:text-base text-[#101828] font-semibold mb-1">
-                        Multi-functional tool
-                      </h3>
-                      <p className="text-xs text-[#475467] md:text-sm font-normal">
-                        A new way to discover tokens that are safe and
-                        trustworthy.
-                      </p>
+                  <div className="relative">
+                    <span className="absolute right-0 -top-2 md:top-0 text-[rgb(181,71,8)] bg-[rgb(255,250,235)] px-2 ml-0 text-[10px] font-medium rounded">
+                      Beta
+                    </span>
+                    <div className="md:flex gap-4 px-2 md:px-4 py-2 text-sm">
+                      <img
+                        className="inline-block w-12 mx-auto md:mx-0"
+                        src={multiFunctional}
+                        alt="multi functional icon"
+                      />
+                      <div className="md:mt-3">
+                        <h3 className="md:text-base text-[#101828] font-semibold mb-1">
+                          Multi-functional tool
+                        </h3>
+                        <p className="text-xs text-[#475467] md:text-sm font-normal">
+                          A new way to discover tokens that are safe and
+                          trustworthy.
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </Menu.Item>
@@ -104,25 +106,27 @@ const ProjectsDropdown = () => {
               </div>
               <div>
                 {/*left menu items */}
-                <span className="text-[rgb(2,122,72)] bg-[rgb(236,253,243)] px-2 ml-0 mr-1 md:mr-0 text-[10px] font-medium float-right rounded">
-                  <span className="w-[6px] h-[6px] bg-[rgb(18,183,106)] inline-block rounded-full mr-1"></span>
-                  Launched
-                </span>
                 <Menu.Item>
-                  <div className="md:flex text-gray-700 gap-4 px-2 md:px-4 py-2 text-sm clear-both">
-                    <img
-                      className="inline-block w-12 mx-auto md:mx-0"
-                      src={cryptoAcademy}
-                      alt="crypto academy icon"
-                    />
-                    <div className="md:mt-3">
-                      <h3 className="md:text-base text-[#101828] font-semibold mb-1">
-                        Crypto academy
-                      </h3>
-                      <p className="text-xs text-[#475467] md:text-sm font-normal">
-                        A new way to discover tokens that are safe and
-                        trustworthy.
-                      </p>
+                  <div className="relative">
+                    <span className="absolute right-0 -top-2 md:top-0 text-[rgb(2,122,72)] bg-[rgb(236,253,243)] px-2 ml-0 mr-1 md:mr-0 text-[10px] font-medium rounded">
+                      <span className="w-[6px] h-[6px] bg-[rgb(18,183,106)] inline-block rounded-full mr-1"></span>
+                      Launched
+                    </span>
+                    <div className="md:flex text-gray-700 gap-4 px-2 md:px-4 py-2 text-sm">
+                      <img
+                        className="inline-block w-12 mx-auto md:mx-0"
+                        src={cryptoAcademy}
+                        alt="crypto academy icon"
+                      />
+                      <div className="md:mt-3">
+                        <h3 className="md:text-base text-[#101828] font-semibold mb-1">
+                          Crypto academy
+                        </h3>
+                        <p className="text-xs text-[#475467] md:text-sm font-normal">
+                          A new way to discover tokens that are safe and
+                          trustworthy.
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </Menu.Item>
