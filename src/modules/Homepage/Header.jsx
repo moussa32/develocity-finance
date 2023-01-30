0e3fac05cb631ff52fc83a5155acb8ef52dd5709
mozes32 <moussaebrahem32@gmail.com>
2023-01-30 12:04:07 +0200

    fix: issues in header

--- src/modules/Homepage/Header.jsx
+++ src/modules/Homepage/Header.jsx
@@ -1,30 +1,13 @@
 import { useEffect, useState } from "react";
-import LadingHeaderBG from "../../../src/assets/images/ladingHeaderBG.png";
-import BloombergIcon from "../../../src/assets/images/Bloomberg.svg";
-import WiredIcon from "../../../src/assets/images/WIRED.svg";
-import CoindeskIcons from "../../../src/assets/images/Coindesk.svg";
-import YahooFinanceIcons from "../../../src/assets/images/YahooFinance.svg";
-import Loudspeaker from "../../../src/assets/images/loudspeaker.svg";
+import BloombergIcon from "../../assets/images/Bloomberg.svg";
+import WiredIcon from "../../assets/images/WIRED.svg";
+import CoindeskIcons from "../../assets/images/Coindesk.svg";
+import YahooFinanceIcons from "../../assets/images/YahooFinance.svg";
+import Loudspeaker from "../../assets/images/loudspeaker.svg";
 import SideMenu from "../../shared/Components/SideMenu";
+import { calculateTimeLeft } from "../../shared/Util/countdown";
 
-export default function Header() {
-  const calculateTimeLeft = () => {
-    let year = new Date().getFullYear();
-    let difference = +new Date(`2/10/${year}`) - +new Date();
-
-    let timeLeft = {};
-
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
-    }
-
-    return timeLeft;
-  };
+const Header = () => {
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
   useEffect(() => {
@@ -36,26 +19,30 @@ export default function Header() {
   });
 
   return (
-    <div className="relative main-bg bg-center bg-no-repeat text-center overflow-hidden w-full md:bg-cover md:bg-right md:text-left md:h-[810px]">
-      <img src={LadingHeaderBG} className="absolute h-full w-full z-0" />
-      {/* <div className="fixed w-full z-40">
-        <Navbar containerClassName={"bg-opacity-0"} />
-      </div> */}
+    <div className="relative header-bg bg-cover bg-center bg-no-repeat text-center overflow-hidden w-full md:bg-cover md:bg-right md:text-left md:h-[810px]">
       <SideMenu />
-      <div className="container mx-auto text-white mt-64 z-20 flex flex-col">
-        <div className="w-50 md:w-[615px] md:h-[400px] flex flex-col justify-center z-20">
+      <div className="container mx-auto text-white mt-60 z-20 flex flex-col">
+        <div className="w-50 mx-auto flex flex-col justify-center z-20 md:h-[400px] lg:px-8 lg:ml-0 xl:w-[615px] xl:px-0">
           <div className="flex flex-col justify-center">
-            <h2 className="md:text-6xl text-4xl mb-6">Pre-Sale Countdown</h2>
-            <time className="md:text-6xl text-4xl text-center mt-[30px]">
+            <h2 className="font-PolySans text-4xl mb-6 md:mb-0 sm:text-5xl md:text-6xl">
+              Pre-Sale Countdown
+            </h2>
+            <time className="font-semibold text-4xl text-center md:mt-[29px] sm:text-5xl md:text-7xl">
               {`${timeLeft.days}:${timeLeft.hours}:${timeLeft.minutes}:${timeLeft.seconds}`}
             </time>
           </div>
           <div className=" w-full flex justify-center md:justify-start">
-            <div className="mt-[125px] w-11/12 h-16 bg-gray-500 bg-opacity-25 rounded-lg flex items-center">
-              <div className="ml-3 p-2 rounded-lg  bg-gray-300 bg-opacity-20">
-                <img className="w-[18px] h-[18px]" src={`${Loudspeaker}`} />
+            <div className="px-3.5 mt-[125px] w-11/12 h-16 bg-gray-500 bg-opacity-25 rounded-lg flex items-center gap-3">
+              <div className="flex items-center justify-center rounded-lg w-9 h-9 bg-gray-300 bg-opacity-20 basis-9 shrink-0">
+                <img
+                  className="w-[18px] h-[18px] md:h-auto md:w-auto"
+                  src={`${Loudspeaker}`}
+                />
               </div>
-              <p className="ml-2 text-sm" style={{ color: "#E9E9E9" }}>
+              <p
+                className="ml-2 text-xs xs:text-sm"
+                style={{ color: "#E9E9E9" }}
+              >
                 Sheikh{" "}
                 <span
                   className="underline underline-offset-2"
@@ -69,25 +56,27 @@ export default function Header() {
           </div>
         </div>
 
-        <div className="flex justify-evenly my-14 md:mt-12 mx-auto gap-1 md:gap-10">
+        <div className="mt-20 mb-14 flex flex-wrap justify-evenly mx-auto px-4 gap-x-3 gap-y-6 md:my-14 md:mt-24 md:gap-10 lg:gap-y-0">
           <img
-            className="w-[65px] md:w-[100%] opacity-30 hover:opacity-80"
+            className="max-w-[150px] md:max-w-[100%] opacity-30 hover:opacity-80"
             src={`${BloombergIcon}`}
           />
           <img
-            className="w-[65px] md:w-[100%] opacity-30 hover:opacity-80"
+            className="max-w-[150px] md:max-w-[100%] opacity-30 hover:opacity-80"
             src={`${WiredIcon}`}
           />
           <img
-            className="w-[65px] md:w-[100%] opacity-30 hover:opacity-80"
+            className="max-w-[150px] md:max-w-[100%] opacity-30 hover:opacity-80"
             src={`${CoindeskIcons}`}
           />
           <img
-            className="w-[55px] md:w-[100%] opacity-30 hover:opacity-80"
+            className="max-w-[150px] md:max-w-[100%] opacity-30 hover:opacity-80"
             src={`${YahooFinanceIcons}`}
           />
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Header;
---
+++ src/shared/Util/countdown.js
@@ -0,0 +1,29 @@
+const convertToDoubleNumber = (number) => {
+  if (number < 10) {
+    return `0${number}`;
+  } else {
+    return number;
+  }
+};
+
+export const calculateTimeLeft = () => {
+  let year = new Date().getFullYear();
+  let difference = +new Date(`2/10/${year}`) - +new Date();
+
+  let timeLeft = {};
+
+  if (difference > 0) {
+    timeLeft = {
+      days: convertToDoubleNumber(
+        Math.floor(difference / (1000 * 60 * 60 * 24))
+      ),
+      hours: convertToDoubleNumber(
+        Math.floor((difference / (1000 * 60 * 60)) % 24)
+      ),
+      minutes: convertToDoubleNumber(Math.floor((difference / 1000 / 60) % 60)),
+      seconds: convertToDoubleNumber(Math.floor((difference / 1000) % 60)),
+    };
+  }
+
+  return timeLeft;
+};
