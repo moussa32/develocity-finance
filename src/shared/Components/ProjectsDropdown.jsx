94d6f1b6604e3e75df945d474107a1639b030a3b
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2023-01-30 11:46:17 +0200

    edit dropdown arrow position

--- src/shared/Components/ProjectsDropdown.jsx
+++ src/shared/Components/ProjectsDropdown.jsx
@@ -35,7 +35,7 @@ const ProjectsDropdown = () => {
       >
         <Menu.Items className="absolute px-2 md:px-9 -right-[120px] md:-right-96 lg:-right-72 z-10 md:pb-8 mt-5 w-[325px] md:w-[680px] origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <img
-            className="absolute right-36 md:right-[425px] lg:right-80 -top-[7px] origin-top-right"
+            className="absolute right-40 md:right-[425px] lg:right-80 -top-[7px] origin-top-right"
             src={dropdownArrow}
             alt="dropdown arrow"
           />
