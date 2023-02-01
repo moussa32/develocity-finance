bfb8b1e887bece8f14e64a236cf21d0f6bb57628
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2023-02-01 13:28:30 +0200

    edit blog in mobile screen

--- src/modules/Homepage/Blog.jsx
+++ src/modules/Homepage/Blog.jsx
@@ -4,23 +4,23 @@ import { Link } from "react-router-dom";
 
 function Blog() {
   return (
-    <div className="text-center my-32">
-      <div className=" container mx-auto">
-        <h3 className="font-PolySans text-6xl  text-[#29233B]">
+    <div className="text-center mt-28 mb-[59px] md:mt-32 md:mb-32">
+      <div className="container mx-auto">
+        <h3 className="font-PolySans text-[28px] text-[#171717] md:text-6xl  md:text-[#29233B]">
           Develocity Blog
         </h3>
-        <p className="text-lg mt-4 capitalize text-[#525C7A]">
+        <p className="text-base text-[#525252] md:text-lg mt-3 md:mt-4 md:capitalize md:text-[#525C7A]">
           News, updates, and useful content from across the Develocity
           Ecosystem.
         </p>
         <div className="flex justify-center items-center">
           <div className=" mt-8 gap-9 grid grid-cols-1 lg:grid-cols-2">
             <div className="shadow-xl bg-slate-50 flex flex-col justify-center rounded-lg overflow-hidden h-[650px] max-w-[488px]">
-              <div className="text-left ml-8 mt-8 flex flex-col">
-                <h3 className="font-sans text-3xl font-medium capitalize text-[#29233B]">
-                  We are launching App for Multi-functional Tool
+              <div className="text-left ml-4 md:ml-8 mt-8 flex flex-col">
+                <h3 className="font-sans text-[28px] font-semibold px-2 md:px-0 md:text-3xl md:font-medium capitalize text-[#29233B]">
+                  We are launching <span className="hidden md:inline-block">App for</span> Multi-functional Tool
                 </h3>
-                <p className="text-base mt-3 text-[#525C7A]">
+                <p className="text-base mt-3 pl-2 md:pl-0 text-[#525252] md:text-[#525C7A]">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
                   standard dummy text ever since the 1500s, when an unknown
@@ -29,40 +29,40 @@ function Blog() {
                 </p>
                 <Link
                   to={"blog"}
-                  className="mt-7 font-medium flex gap-2 text-indigo-500"
+                  className="mt-10 md:mt-7 pl-2 md:pl-0 font-medium flex gap-2 text-indigo-500"
                 >
                   Learn More &#10140;
                 </Link>
               </div>
-              <div className="flex justify-center h-[700px]">
-                <div className="relative w-4/6">
-                  <div className="absolute top-20 md:top-6 w-full">
+              <div className="flex justify-center h-[280px] md:h-[700px]">
+                <div className="relative w-5/6 md:w-4/6">
+                  <div className="absolute top-12 md:top-6 w-full">
                     <img className="w-full" src={phoneImage} />
                   </div>
                 </div>
               </div>
             </div>
 
-            <div className="shadow-xl bg-slate-50	 flex flex-col pb-10 rounded-lg h-[650px] max-w-[488px]">
+            <div className="shadow-xl bg-slate-50	 flex flex-col pb-6 md:pb-10 rounded-lg h-[650px] max-w-[488px]">
               <div className="realtive flex justify-center bg-gradient-to-r from-[#120734] to-[#000000] rounded-t-lg h-[382px]">
                 <div className="absolute self-end ">
                   <img className="w-10/12" src={manImage} />
                 </div>
               </div>
-              <div className="text-left max-w-[424px] ml-8 mt-3 flex flex-col ">
-                <h3 className="font-sans text-3xl mt-3 font-medium capitalize  text-[#29233B]">
-                  Discover the Business Solutions we have for you
+              <div className="text-left max-w-[424px] ml-4 md:ml-8 mt-3 flex flex-col ">
+                <h3 className="font-sans text-[28px] font-semibold md:text-3xl mt-3 pl-2 md:pl-0 md:font-medium capitalize  text-[#29233B]">
+                  Discover the Business Solutions we have <span className="hidden md:inline-block">for you</span>
                 </h3>
-                <p className="text-base mt-4  text-[#525C7A]">
+                <p className="text-base mt-3 md:mt-4 pl-2 md:pl-0 text-[#525C7A]">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
+                  standard dummy text ever since the<span className="hidden md:inline-block"> 1500s, when an unknown
                   printer took a galley of type and scrambled it to make a type
-                  specimen book.
+                  specimen book</span>.
                 </p>
                 <Link
                   to={"blog"}
-                  className="mt-7 font-medium flex gap-2 text-indigo-500"
+                  className="mt-4 md:mt-7 font-medium pl-2 md:pl-0 flex gap-2 text-indigo-500"
                 >
                   Learn More &#10140;
                 </Link>
@@ -70,7 +70,7 @@ function Blog() {
             </div>
           </div>
         </div>
-        <div className="mt-[60px] text-indigo-500 font-semibold">
+        <div className="text-lg mt-[46px] md:mt-[60px] text-indigo-500 font-semibold">
           <Link to={"terms-and-conditions"}>
             Check more news and updates &#10140;
           </Link>
