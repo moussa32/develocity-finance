24dc0993bf6db599bc559886832cd7941576a278
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2023-02-01 14:34:55 +0200

    edit leadership design in large screen

--- src/modules/Homepage/Leadership.jsx
+++ src/modules/Homepage/Leadership.jsx
@@ -6,20 +6,20 @@ function Leadership() {
   return (
     <div className="text-center my-24">
       <div className="font-PolySans container mx-auto">
-        <p className="text-6xl text-indigo-900" style={{ color: "#29233B" }}>
+        <p className="text-[28px] md:text-5xl text-[#171717]">
           Develocity Leadership
         </p>
-        <p className="text-lg mt-2" style={{ color: "#525C7A" }}>
+        <p className="text-base md:text-lg mt-2 text-[#525252] md:text-[#404040]">
           With the mindset to create secure future in the digital money.
         </p>
-        <div className="grid mt-6 gap-6 grid-cols-1 lg:grid-cols-3">
-          <div className="relative mx-8 px-6 pt-[27px] pb-[27px] rounded-xl bg-gradient-to-t from-[#280D46] to-[#DC88F9] h-96">
-            <div className="absolute bottom-0 right-0 left-0 bg-slate-400 opacity-25 z-0  h-24"></div>
+        <div className="grid mt-14 gap-7 grid-cols-1 lg:grid-cols-3">
+          <div className="relative mx-8 px-6 pt-[27px] pb-[27px] rounded-xl bg-gradient-to-br from-[#1D2B64] to-[#F8CDDA] lg:w-[26.6vw] h-[484px]">
+            <div className="absolute bottom-0 right-0 left-0 bg-white opacity-10 z-0 h-28"></div>
             <div className="absolute bottom-0 right-0 left-0 z-20">
-              <div className="px-3 pt-4 pb-2 text-left text-white z-50">
+              <div className="px-6 pt-6 pb-6 text-left text-white z-50">
                 <p className="text-2xl font-medium">Mahdi Ajaj</p>
-                <div className="flex justify-between mt-2">
-                  <label className="text-xl	">CEO</label>
+                <div className="flex justify-between mt-1">
+                  <label className="text-[22px]">CEO</label>
                   <div className="flex gap-2">
                     <img className="w-5" src={facebook} />
                     <img className="w-5" src={twitter} />
@@ -29,13 +29,13 @@ function Leadership() {
               </div>
             </div>
           </div>
-          <div className="relative mx-8 px-6 pt-[27px] pb-[27px] rounded-xl bg-gradient-to-t from-[#101828] to-[#8DA4C9] h-96">
-            <div className="absolute bottom-0 right-0 left-0 bg-slate-400 opacity-25 z-0  h-24"></div>
+          <div className="relative mx-8 px-6 pt-[27px] pb-[27px] rounded-xl bg-gradient-to-br from-[#1D2B64] to-[#F8CDDA] lg:w-[26.6vw] h-[484px]">
+            <div className="absolute bottom-0 right-0 left-0 bg-white opacity-10 z-0 h-28"></div>
             <div className="absolute bottom-0 right-0 left-0 z-20">
-              <div className="px-3 pt-4 pb-2 text-left text-white z-50">
+              <div className="px-6 pt-6 pb-6 text-left text-white z-50">
                 <p className="text-2xl font-medium mb-1">Abdulaziz Bin Fahad</p>
-                <div className="flex justify-between mt-2">
-                  <label className="text-xl	">CTO</label>
+                <div className="flex justify-between mt-1">
+                  <label className="text-[22px]">CTO</label>
                   <div className="flex gap-2">
                     <img className="w-5" src={facebook} />
                     <img className="w-5" src={twitter} />
@@ -45,15 +45,15 @@ function Leadership() {
               </div>
             </div>
           </div>
-          <div className="relative mx-8 px-6 pt-[27px] pb-[27px] rounded-xl bg-gradient-to-t from-[#030A13] to-[#859AFF] h-96">
-            <div className="absolute bottom-0 right-0 left-0 bg-slate-400 opacity-25 z-0  h-24"></div>
+          <div className="relative mx-8 px-6 pt-[27px] pb-[27px] rounded-xl bg-gradient-to-br from-[#1D2B64] to-[#F8CDDA] lg:w-[26.6vw] h-[484px]">
+            <div className="absolute bottom-0 right-0 left-0 bg-white opacity-10 z-0 h-28"></div>
             <div className="absolute bottom-0 right-0 left-0 z-20">
-              <div className="px-3 pt-4 pb-2 text-left text-white z-50">
+              <div className="px-6 pt-6 pb-6 text-left text-white z-50">
                 <p className="mb-2 text-2xl font-medium">
                   Mamuka Kavtelishvili
                 </p>
-                <div className="flex justify-between mt-2">
-                  <label className="text-xl ">CMO</label>
+                <div className="flex justify-between mt-1">
+                  <label className="text-[22px]">CMO</label>
                   <div className="flex gap-2">
                     <img className="w-5" src={facebook} />
                     <img className="w-5" src={twitter} />
