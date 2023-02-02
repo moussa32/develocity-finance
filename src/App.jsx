835502aeabcc6369ba7df5d3c6795afe229875c8
mozes32 <moussaebrahem32@gmail.com>
2023-02-02 13:01:15 +0200

    fix: routing error

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ const router = createBrowserRouter([
     path: "/",
     children: [
       { path: "/", element: <Home /> },
-      { path: "/blog", element: <Blog /> },
-      { path: "/blog/:id", element: <BlogDetails /> },
-      { path: "/terms-and-conditions", element: <TermsConditions /> },
-      { path: "/contact-us", element: <Contact /> },
+      { path: "blog", element: <Blog /> },
+      { path: "blog/:id", element: <BlogDetails /> },
+      { path: "terms-and-conditions", element: <TermsConditions /> },
+      { path: "contact-us", element: <Contact /> },
     ],
   },
 ]);
