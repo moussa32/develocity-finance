e24e0b3c3aec9ad8126ec4291164be1941357107
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2023-02-13 14:21:03 +0200

    add translation to footer

--- shared/Components/Footer.jsx
+++ shared/Components/Footer.jsx
@@ -5,8 +5,10 @@ import instagram from "../../public/assets/images/instagram.svg";
 import telegram from "../../public/assets/images/telegram.svg";
 import Image from "next/image";
 import NextNavLink from "./NextNavLink";
+import useTranslation from "@/shared/Hooks/useTranslation";
 
 const Footer = () => {
+  const { t, errors } = useTranslation("footer");
   return (
     <div className="bg-[#F9FAFB]">
       <div className="container mx-auto md:px-10 lg:px-0 pt-16 md:pt-24 pb-16 text-left text-[#525C7A] text-base font-medium">
@@ -14,30 +16,42 @@ const Footer = () => {
           <div>
             <Image className="" src={logo} alt="footer logo" />
             <div className="mt-6">
-              <NextNavLink href="/" className="md:block lg:inline-block mr-6 md:mr-0 lg:mr-6 mt-6 lg:mt-0">
-                Returns & FAQ
+              <NextNavLink
+                href="/"
+                className="md:block lg:inline-block mr-6 md:mr-0 lg:mr-6 mt-6 lg:mt-0"
+              >
+                {t?.links?.returnsFAQ}
               </NextNavLink>
-              <NextNavLink href="/" className="md:block lg:inline-block mr-6 md:mr-0 lg:mr-6 mt-6 lg:mt-0">
-                Privacy Policy
+              <NextNavLink
+                href="/terms-and-conditions"
+                className="md:block lg:inline-block mr-6 md:mr-0 lg:mr-6 mt-6 lg:mt-0"
+              >
+                {t?.links?.privacyPolicy}
               </NextNavLink>
-              <NextNavLink href="/contact-us" className="md:block lg:inline-block mr-6 md:mr-0 lg:mr-6 mt-6 lg:mt-0">
-                Contact
+              <NextNavLink
+                href="/contact-us"
+                className="md:block lg:inline-block mr-6 md:mr-0 lg:mr-6 mt-6 lg:mt-0"
+              >
+                {t?.links?.contact}
               </NextNavLink>
-              <NextNavLink href="/" className="hidden md:block lg:inline-block lg:mr-6 my-6 lg:my-0">
-                Back to Develocity
+              <NextNavLink
+                href="/"
+                className="hidden md:block lg:inline-block lg:mr-6 my-6 lg:my-0"
+              >
+                {t?.links?.backToDevelocity}
               </NextNavLink>
             </div>
           </div>
           <div className="hidden md:block ml-auto">
-            <p className="px-5 md:px-0">Get the latest updates about Develocity's new projects,</p>
-            <p className="px-5 md:px-0">roadmap updates and company news.</p>
+            <p className="px-5 md:px-0">{t?.input?.mainText1}</p>
+            <p className="px-5 md:px-0">{t?.input?.mainText2}</p>
             <div className="mt-6 flex flex-col md:flex-row items-center flex-wrap md:mt-4">
               <input
                 className="placeholder:text-[#667085] font-normal placeholder:text-base h-11 w-72 md:w-80 border-1 border py-5 md:mr-2 pl-3 border-[#D0D5DD]"
-                placeholder="Enter your email..."
+                placeholder={t?.input?.placeholder}
               />
               <button className="flex items-center justify-center bg-indigo-500 h-[44px] rounded-sm text-white font-normal py-3 px-6 ml-2 mt-5 md:mx-auto lg:mt-0 hover:bg-indigo-600">
-                Subscribe
+                {t?.input?.subscribeBtn}
               </button>
             </div>
           </div>
@@ -46,19 +60,37 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-2">
           <div>
             <a href="#">
-              <Image className="inline-block ml-4" src={facebook} alt="facebook icon" />
+              <Image
+                className="inline-block ml-4"
+                src={facebook}
+                alt="facebook icon"
+              />
             </a>
             <a href="#">
-              <Image className="inline-block ml-4" src={twitter} alt="twitter icon" />
+              <Image
+                className="inline-block ml-4"
+                src={twitter}
+                alt="twitter icon"
+              />
             </a>
             <a href="#">
-              <Image className="inline-block ml-4" src={instagram} alt="instagram icon" />
+              <Image
+                className="inline-block ml-4"
+                src={instagram}
+                alt="instagram icon"
+              />
             </a>
             <a href="#">
-              <Image className="inline-block ml-4" src={telegram} alt="telegram icon" />
+              <Image
+                className="inline-block ml-4"
+                src={telegram}
+                alt="telegram icon"
+              />
             </a>
           </div>
-          <p className="mt-6 md:mt-0 ml-4 md:ml-0 md:text-right">© 2022 Develocity, LLC. All Rights Reserved</p>
+          <p className="mt-6 md:mt-0 ml-4 md:ml-0 md:text-right">
+            {t?.copyright}
+          </p>
         </div>
       </div>
     </div>
