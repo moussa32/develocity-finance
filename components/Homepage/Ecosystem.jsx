23ae109dc091b613d8a8109d9e6a2a410c3001ac
mozes32 <moussaebrahem32@gmail.com>
2023-03-06 17:23:48 +0200

    fix: remove duplicated playsInline attribute

--- components/Homepage/Ecosystem.jsx
+++ components/Homepage/Ecosystem.jsx
@@ -25,46 +25,28 @@ const Ecosystem = () => {
             <span className="bg-white py-0.5 px-2 md:py-1 md:px-2.5 rounded-2xl">
               {t?.homeSection?.ecoSystemSection?.lableI}
             </span>
-            <span className="text-[#344054]">
-              {t?.homeSection?.ecoSystemSection?.lableII}
-            </span>
+            <span className="text-[#344054]">{t?.homeSection?.ecoSystemSection?.lableII}</span>
           </span>
           <h3 className="text-[#101828] text-center font-PolySans text-[28px] font-bold mt-3 mb-4 md:mb-6 ltr:md:text-left rtl:md:text-right md:font-sans md:text-2xl lg:text-4xl">
             {t?.homeSection?.ecoSystemSection?.mainText}
           </h3>
-          <p className="text-neutral-600 text-base">
-            {t?.homeSection?.ecoSystemSection?.subText}
-          </p>
-          <p className="text-neutral-600 text-base mt-4">
-            {t?.homeSection?.ecoSystemSection?.subTextSecondParagraph}
-          </p>
+          <p className="text-neutral-600 text-base">{t?.homeSection?.ecoSystemSection?.subText}</p>
+          <p className="text-neutral-600 text-base mt-4">{t?.homeSection?.ecoSystemSection?.subTextSecondParagraph}</p>
           <div className="flex flex-wrap lg:grid lg:grid-cols-2 mt-8 gap-y-4">
             <div className="flex items-center w-3/5 ltr:mr-5 rtl:ml-5 ltr:lg:mr-0 rtl:lg:ml-0 lg:w-full">
-              <Image
-                src={MultiFunctionalIcon}
-                className="w-8 h-8"
-                alt="MultiFunctionalIcon"
-              />
+              <Image src={MultiFunctionalIcon} className="w-8 h-8" alt="MultiFunctionalIcon" />
               <h4 className="ltr:ml-2 rtl:mr-2 text-[#171717] font-medium text-base">
                 {t?.homeSection?.ecoSystemSection?.tools?.multiFunctionalTool}
               </h4>
             </div>
             <div className="flex items-center">
-              <Image
-                src={SwapToolIcon}
-                className="w-8 h-8"
-                alt="SwapToolIcon"
-              />
+              <Image src={SwapToolIcon} className="w-8 h-8" alt="SwapToolIcon" />
               <h4 className="ltr:ml-2 rtl:mr-2 text-[#171717] font-medium text-base">
                 {t?.homeSection?.ecoSystemSection?.tools?.swapTool}
               </h4>
             </div>
             <div className="flex items-center w-3/5 ltr:mr-5 rtl:ml-5 ltr:lg:mr-0 rtl:lg:ml-0 lg:w-full">
-              <Image
-                src={CryptoAcademyIcon}
-                className="w-8 h-8"
-                alt="CryptoAcademyIcon"
-              />
+              <Image src={CryptoAcademyIcon} className="w-8 h-8" alt="CryptoAcademyIcon" />
               <h4 className="ltr:ml-2 rtl:mr-2 text-[#171717] font-medium text-base">
                 {t?.homeSection?.ecoSystemSection?.tools?.wallet}
               </h4>
@@ -95,7 +77,6 @@ const Ecosystem = () => {
           playsInline
           autoPlay
           muted
-          playsInline
         ></video>
 
         {/*   <Image
