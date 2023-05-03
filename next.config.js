37c71bdd29f38848597ba351c173f7e2287241db
mozes32 <moussaebrahem32@gmail.com>
2023-05-03 14:36:48 +0300

    fix: add support to jsx

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withPlugins = require("next-compose-plugins");
 
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["jsx"],
   i18n: {
     locales: ["en", "ar", "tr", "ru", "ka", "zh"],
     defaultLocale: "en",
