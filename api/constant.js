ad526605f174bb921a14a61583a156c70eea3036
mozes32 <moussaebrahem32@gmail.com>
2023-05-09 16:24:55 +0300

    refactor: change blog url

--- api/constant.js
+++ api/constant.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://blog.develocity.app/api";
+const baseURL = "https://phplaravel-992545-3494971.cloudwaysapps.com/api";
 const app_api_key = "CwIX5kPsfTrlDHQZxK3yW6SlNPqjA8StUhZ0xeEV6dDw0kgOmwxS2pnxLN1FBPZL";
 
 export const globalInstance = axios.create({
