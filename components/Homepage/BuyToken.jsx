d95882a78188047a90be1e8b116aaab061e1aa90
AbdulazizBinFahad <94074156+AbdulazizBinFahad@users.noreply.github.com>
2023-05-12 16:06:33 +0300

    .

--- components/Homepage/BuyToken.jsx
+++ components/Homepage/BuyToken.jsx
@@ -28,7 +28,7 @@ const BuyToken = ({ handleBuyNowButton, openAllStagesModal }) => {
           <h3 className="font-medium text-sm md:text-lg">
             {progress}% <span className="text-indigo-500">{t?.title?.SOLD}</span>
           </h3>
-          <h3 className="font-medium text-sm md:text-lg">2,400,000 {t?.title?.tokensRe}</h3>
+          <h3 className="font-medium text-sm md:text-lg">2,315,000 {t?.title?.tokensRe}</h3>
         </div>
         <ProgressBar value={SOLD_PERCENTAGE} />
         <div className="flex items-center flex-col gap-2 bg-indigo-500/10 border-indigo-500/30 border-1 rounded-xl px-4 py-[15px] sm:gap-0 sm:flex-row justify-center md:justify-between md:px-6">
