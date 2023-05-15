06f624a30b02d4a517d15cca89239215b2ebf7ac
mozes32 <moussaebrahem32@gmail.com>
2023-05-15 17:33:59 +0300

    feat: added google analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -20,6 +20,21 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <NextScript
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <NextScript strategy="lazyOnload">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+        </NextScript>
         <meta property="og:image" content="/apple-icon-114x114.png" />
         <meta property="og:image:width" content="114" />
         <meta property="og:image:height" content="114" />
@@ -27,7 +42,6 @@ export default function Document() {
         <link rel="apple-touch-icon" href="favicon.jpg" />
         <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
         <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-
       </Head>
       <body>
         <Main />
