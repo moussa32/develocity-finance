5d21afd3b0e481249cbbf8c7b7362e8153390ef7
AbdulazizBinFahad <94074156+AbdulazizBinFahad@users.noreply.github.com>
2023-05-16 03:28:38 +0300

    Update fetchPreSaleStatus.js

--- api/fetchPreSaleStatus.js
+++ api/fetchPreSaleStatus.js
@@ -3,6 +3,5 @@ import { globalInstance , emailInstance} from "@/api/constant";
 export const fetchPreSaleStatus = async () => {
   const fetRequest = await emailInstance.get("/v1/static/totalTokensData");
   const { data } = fetRequest;
-  console.log(data);
   return data;
 };
