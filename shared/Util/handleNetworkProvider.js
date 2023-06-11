2438fbb39cd89dcdd85969a1eac93c2e4ca40ed3
AbdulazizBinFahad <94074156+AbdulazizBinFahad@users.noreply.github.com>
2023-06-12 01:43:15 +0300

    Update handleNetworkProvider.js

--- shared/Util/handleNetworkProvider.js
+++ shared/Util/handleNetworkProvider.js
@@ -48,7 +48,7 @@ export const getMainCoinContractAddress = network => {
     case "homestead":
       return "0x981342751d7b08e704a4b208F9e4c720b981B4E1";
     case "bsc":
-      return "0x2F7f89d52131c3cd24eD1bb59042A16BCf123d5C";
+      return "0x9314964Cf5b1054DE50B5a87b832732272aeE00f";
     case "matic":
       return "0x2F7f89d52131c3cd24eD1bb59042A16BCf123d5C";
   }
