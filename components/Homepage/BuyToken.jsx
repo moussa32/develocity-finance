e279c0122db413823833a6aa39c1ea71de872317
AbdulazizBinFahad <94074156+AbdulazizBinFahad@users.noreply.github.com>
2023-06-11 19:01:02 +0300

    Update BuyToken.jsx

--- components/Homepage/BuyToken.jsx
+++ components/Homepage/BuyToken.jsx
@@ -27,7 +27,7 @@ const BuyToken = ({ handleBuyNowButton, openAllStagesModal }) => {
   });
 
   const { progress } = useProgress({
-    max: data ? Number(Number(data.soldPercentage) + 7.78 ).toFixed(2) : SOLD_PERCENTAGE,
+    max: data ? Number(Number(data.soldPercentage) + 14.03 ).toFixed(2) : SOLD_PERCENTAGE,
   });
 
   return (
@@ -43,7 +43,7 @@ const BuyToken = ({ handleBuyNowButton, openAllStagesModal }) => {
             {progress}% <span className="text-indigo-500">{t?.title?.SOLD}</span>
           </h3>
           <h3 className="font-medium text-sm md:text-lg">
-            ${new Intl.NumberFormat('en-US').format(data ? Number(Number(data.totalSOLDOnUSD).toFixed(0)) + 77837 : 0)} /
+            ${new Intl.NumberFormat('en-US').format(data ? Number(Number(data.totalSOLDOnUSD).toFixed(0)) + 140343 : 0)} /
             $1,000,000
           </h3>
         </div>
@@ -74,7 +74,7 @@ const BuyToken = ({ handleBuyNowButton, openAllStagesModal }) => {
         )}
 
         <ProgressBar
-          value={data ? Number(Number(data.soldPercentage) + 7.78).toFixed(2) : SOLD_PERCENTAGE}
+          value={data ? Number(Number(data.soldPercentage) + 14.03).toFixed(2) : SOLD_PERCENTAGE}
           text={<p className="w-fit">{t?.counter?.nextPrice} ${NEXT_STAGE_PRICE}</p>}
         />
 
