150f4c9d2b4eec14d756a96e841ea57279c18606
Sikkaplus-frontend <moussaebrahem32@gmail.com>
2023-07-23 15:11:04 +0300

    refactor: update api base url

--- api/constant.js
+++ api/constant.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://phplaravel-992545-3494971.cloudwaysapps.com/api";
+const baseURL = "https://phplaravel-1066800-3734034.cloudwaysapps.com/api";
 const emailURL = "https://powerful-teeth-production.up.railway.app/";
 const app_api_key = "CwIX5kPsfTrlDHQZxK3yW6SlNPqjA8StUhZ0xeEV6dDw0kgOmwxS2pnxLN1FBPZL";
 
@@ -8,7 +8,6 @@ export const globalInstance = axios.create({
   baseURL: baseURL,
   headers: { "app-api-key": app_api_key },
 });
-
 
 export const emailInstance = axios.create({
   baseURL: emailURL,
