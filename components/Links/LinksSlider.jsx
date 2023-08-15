775c8c371d768d2eeef86d067e633214a75f0f2e
Moussa Ibrahim <moussaebrahem32@gmail.com>
2023-08-15 09:39:09 +0300

    refactor: add UI changes

---
+++ components/Links/LinksSlider.jsx
@@ -0,0 +1,78 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
+import { useCallback, useRef } from "react";
+import { useWindowSize } from "react-use";
+import useDirection from "@/store/direaction";
+import copy from "copy-to-clipboard";
+import Image from "next/image";
+
+const LinksSlider = ({ urls, activeLink, changeActiveLink }) => {
+  const swiperNavPrevRef = useRef(null);
+  const swiperNavNextRef = useRef(null);
+  const { width } = useWindowSize();
+  const { direction } = useDirection(state => state);
+
+  const selectLink = useCallback(
+    linkIndex => {
+      if (activeLink.linkCopiedIndex === linkIndex) {
+        changeActiveLink({ linkCopiedIndex: null, copied: false });
+      } else {
+        changeActiveLink({ linkCopiedIndex: linkIndex, copied: true });
+        copy(urls[linkIndex].url);
+        window.open(urls[linkIndex].url);
+      }
+    },
+    [activeLink.linkCopiedIndex]
+  );
+
+  return (
+    <Swiper
+      className="mt-12 mb-[23.3px] h-[440px] [&_.swiper-pagination-bullet]:w-[15px] [&_.swiper-pagination-bullet]:h-[15px] [&_.swiper-pagination-bullet]:bg-white [&_.swiper-pagination]:pt-5"
+      modules={[Navigation, Pagination]}
+      key={direction}
+      navigation={{
+        prevEl: swiperNavPrevRef.current,
+        nextEl: swiperNavNextRef.current,
+      }}
+      pagination={{ clickable: true }}
+      speed={800}
+      spaceBetween={width <= 1024 ? 45 : direction === "rtl" ? 0 : 45}
+      slidesPerView={width <= 728 ? 1 : 2}
+      onInit={swiper => {
+        swiper.params.navigation.prevEl = swiperNavPrevRef.current;
+        swiper.params.navigation.nextEl = swiperNavNextRef.current;
+        swiper.navigation.init();
+        swiper.navigation.update();
+      }}
+    >
+      {urls.map((link, index) => (
+        <SwiperSlide key={`${link.linkCopiedIndex}${index}`}>
+          <button
+            onClick={() => selectLink(index)}
+            className={`${
+              activeLink.linkCopiedIndex === index ? "border-opacity-100" : "border-opacity-20"
+            } h-[394px] w-full xl:w-[415.3px] border-white border-1 text-2xl xl:text-[34.54px] relative py-[25.3px] pt-0 rounded-2xl flex flex-col items-center justify-start bg-white bg-opacity-5`}
+          >
+            {console.log(link.icon)}
+            {link.icon && (
+              <div className="flex items-center justify-center flex-col gap-8">
+                <Image className="mt-10" src={link.icon} alt={link.text} />
+                <h2 className="text-3xl mt-auto">{link.text}</h2>
+              </div>
+            )}
+
+            {!link.icon && <h2 className="text-3xl mt-auto">{link.text}</h2>}
+
+            <span className="h-[57.52px] w-[57.52px] bg-[#6A65E7] rounded-full mt-auto">
+              {activeLink.linkCopiedIndex === index && (
+                <span className="absolute bg-white z-20 w-[38.4px] h-[38.4px] rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
+              )}
+            </span>
+          </button>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default LinksSlider;
--- pages/links.jsx
+++ pages/links.jsx
@@ -1,23 +1,20 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper";
 import { Tab } from "@headlessui/react";
-import { Fragment, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Fragment, useMemo, useState } from "react";
 import { Open_Sans } from "@next/font/google";
 import { BiLogoFacebook, BiLogoTelegram } from "react-icons/bi";
 import { AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
-import { useWindowSize } from "react-use";
 import Image from "next/image";
 import DeveLogo from "../public/assets/images/links-page-logo.svg";
 import useTranslation from "@/shared/Hooks/useTranslation";
-import useDirection from "@/store/direaction";
 import copyArrowIcon from "../public/assets/images/copy-arrows.svg";
 import successCopyIcon from "../public/assets/images/success-copy.svg";
-import copy from "copy-to-clipboard";
 import dynamic from "next/dynamic";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import SideMenu from "@/shared/Components/SideMenu";
+import LinksSlider from "@/components/Links/LinksSlider";
+import PreSaleLogo from "./../public/logo.png";
 
 const TabHeader = dynamic(() => import("@/components/Links/TabHeader"), {
   ssr: false,
@@ -26,7 +23,7 @@ const TabHeader = dynamic(() => import("@/components/Links/TabHeader"), {
 const openSans = Open_Sans({ subsets: ["latin"], weight: ["600", "700"] });
 
 const URLS = [
-  { url: "https://develocity.finance", text: "develocity.finance" },
+  { icon: PreSaleLogo, url: "https://develocity.finance", text: "Presale Develocity" },
   { url: "https://develocity.solutions", text: "develocity.solutions" },
   { url: "https://develocity.finance", text: "develocity.finance" },
   { url: "https://develocity.solutions", text: "develocity.solutions" },
@@ -36,44 +33,16 @@ const URLS = [
 
 const LinksPage = () => {
   const [activeLink, setActiveLink] = useState({ linkCopiedIndex: null, copied: false });
-  const [swiper, setSwiper] = useState(null);
-  const { width } = useWindowSize();
-
-  const { direction } = useDirection(state => state);
   const { t } = useTranslation("links");
-  const swiperNavPrevRef = useRef(null);
-  const swiperNavNextRef = useRef(null);
-
   const tabsHeader = useMemo(() => [{ title: t?.Develocitys }, { title: t?.socialMedia }], [t]);
-
-  useEffect(() => {
-    if (swiper && !swiper.destroyed) {
-      // Determine the current active slide index
-      const activeSlideIndex = swiper.activeIndex;
-
-      // Update direction and RTL settings
-      swiper.changeLanguageDirection(direction);
-      swiper.rtlTranslate = direction === "rtl";
-
-      swiper.spaceBetween = direction === "rtl" ? 0 : 45;
-
-      // Update the swiper instance and reset the slide position
-      swiper.update();
-      swiper.slideTo(activeSlideIndex, 0);
-    }
-  }, [swiper, direction]);
-
-  const selectLink = useCallback(
-    linkIndex => {
-      if (activeLink.linkCopiedIndex === linkIndex) {
-        setActiveLink({ linkCopiedIndex: null, copied: false });
-      } else {
-        setActiveLink({ linkCopiedIndex: linkIndex, copied: true });
-        copy(URLS[linkIndex].url);
-        window.open(URLS[linkIndex].url);
-      }
-    },
-    [activeLink.linkCopiedIndex]
+  const SOCIAL_MEDIA = useMemo(
+    () => [
+      { name: t?.facebook, icon: BiLogoFacebook, url: "https://facebook.com" },
+      { name: t?.instagram, icon: AiOutlineInstagram, url: "https://instagram.com" },
+      { name: t?.twitter, icon: AiOutlineTwitter, url: "https://x.com" },
+      { name: t?.telegram, icon: BiLogoTelegram, url: "https://telegram.com" },
+    ],
+    [t]
   );
 
   return (
@@ -89,8 +58,8 @@ const LinksPage = () => {
                   {({ selected }) => (
                     <button
                       className={`${
-                        selected ? "border-b-4 border-[#6A65E7] border-opacity-70" : ""
-                      } text-2xl w-full md:text-[34.54px] pb-2 text-white`}
+                        selected ? "bg-indigo-500/50 border-opacity-70" : ""
+                      } outline-none flex items-center border-4 border-[#6A65E7] lg:border-x-0 lg:border-t-0 transition-all duration-150 justify-center text-2xl w-full md:text-3xl py-4 rounded text-white`}
                     >
                       {header.title}
                     </button>
@@ -101,42 +70,7 @@ const LinksPage = () => {
             <Tab.Panels className="mt-20 xl:mt-[142.2px] border-1 w-full xl:w-[968.85px] mx-auto border-[#6A65E7] rounded-3xl pt-10 xl:pt-[54px] px-7 xl:px-[46px] pb-[22px] text-white">
               <TabHeader />
               <Tab.Panel>
-                <Swiper
-                  className="mt-12 mb-[23.3px] h-[440px] [&_.swiper-pagination-bullet]:w-[15px] [&_.swiper-pagination-bullet]:h-[15px] [&_.swiper-pagination-bullet]:bg-white [&_.swiper-pagination]:pt-5"
-                  modules={[Navigation, Pagination]}
-                  navigation={{
-                    prevEl: swiperNavPrevRef.current,
-                    nextEl: swiperNavNextRef.current,
-                  }}
-                  pagination={{ clickable: true }}
-                  onSwiper={swiper => setSwiper(swiper)}
-                  speed={800}
-                  slidesPerView={width <= 728 ? 1 : 2}
-                  onInit={swiper => {
-                    swiper.params.navigation.prevEl = swiperNavPrevRef.current;
-                    swiper.params.navigation.nextEl = swiperNavNextRef.current;
-                    swiper.navigation.init();
-                    swiper.navigation.update();
-                  }}
-                >
-                  {URLS.map((link, index) => (
-                    <SwiperSlide key={`${link.linkCopiedIndex}${index}`}>
-                      <button
-                        onClick={() => selectLink(index)}
-                        className={`${
-                          activeLink.linkCopiedIndex === index ? "border-opacity-100" : "border-opacity-20"
-                        } h-[394px] w-full lg:w-[415.3px] border-white border-1 text-2xl xl:text-[34.54px] relative py-[25.3px] rounded-2xl flex flex-col items-center justify-center bg-white bg-opacity-5`}
-                      >
-                        {link.text}
-                        <span className="h-[57.52px] w-[57.52px] bg-[#6A65E7] rounded-full absolute bottom-8 mt-auto">
-                          {activeLink.linkCopiedIndex === index && (
-                            <span className="absolute bg-white z-20 w-[38.4px] h-[38.4px] rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></span>
-                          )}
-                        </span>
-                      </button>
-                    </SwiperSlide>
-                  ))}
-                </Swiper>
+                <LinksSlider activeLink={activeLink} changeActiveLink={setActiveLink} urls={URLS} />
 
                 <div
                   className={`flex justify-between items-center h-[44.41px] text-[29.58px] w-full xl:w-[386.54px] ${
@@ -153,51 +87,23 @@ const LinksPage = () => {
                 </div>
               </Tab.Panel>
               <Tab.Panel>
-                <div className="flex flex-wrap items-center gap-[75px] mt-[120.9px] mb-[84px] justify-center">
-                  <a
-                    href="https://facebook.com"
-                    rel="noreferrer"
-                    target="_blank"
-                    className="grid grid-cols-1 place-content-center group"
-                  >
-                    <BiLogoFacebook className="bg-[#6A65E7] text-8xl rounded-full mx-auto p-3 group-hover:bg-white group-hover:text-indigo-500" />
-                    <span className={`uppercase mt-[51.6px] ${openSans.className} text-[25.91px] font-semibold`}>
-                      {t?.facebook}
-                    </span>
-                  </a>
-                  <a
-                    href="https://INSTAGRAM.com"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="grid grid-cols-1 place-content-center group"
-                  >
-                    <AiOutlineInstagram className="bg-[#6A65E7] text-8xl rounded-full mx-auto p-3 group-hover:bg-white group-hover:text-indigo-500" />
-                    <span className={`uppercase mt-[51.6px] ${openSans.className} text-[25.91px] font-semibold`}>
-                      {t?.instagram}
-                    </span>
-                  </a>
-                  <a
-                    href="https://x.com"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="grid grid-cols-1 place-content-center group"
-                  >
-                    <AiOutlineTwitter className="bg-[#6A65E7] text-8xl rounded-full mx-auto p-3 group-hover:bg-white group-hover:text-indigo-500" />
-                    <span className={`uppercase mt-[51.6px] ${openSans.className} text-[25.91px] font-semibold`}>
-                      {t?.twitter}
-                    </span>
-                  </a>
-                  <a
-                    href="https://Telegram.com"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="grid grid-cols-1 place-content-center group"
-                  >
-                    <BiLogoTelegram className="bg-[#6A65E7] text-8xl rounded-full mx-auto p-3 group-hover:bg-white group-hover:text-indigo-500" />
-                    <span className={`uppercase mt-[51.6px] ${openSans.className} text-[25.91px] font-semibold`}>
-                      {t?.telegram}
-                    </span>
-                  </a>
+                <div className="flex flex-wrap items-center gap-y-10 gap-x-5 lg:gap-[75px] mt-[120.9px] mb-[84px] justify-center">
+                  {SOCIAL_MEDIA.map((item, index) => (
+                    <a
+                      key={`${item.name}${index}`}
+                      href={item.url}
+                      rel="noreferrer"
+                      target="_blank"
+                      className="grid grid-cols-1 place-content-center group"
+                    >
+                      <item.icon className="bg-[#6A65E7] text-6xl lg:text-8xl rounded-full mx-auto p-3 group-hover:bg-white group-hover:text-indigo-500" />
+                      <span
+                        className={`uppercase mt-6 lg:mt-[51.6px] ${openSans.className} text-base lg:text-[25.91px] font-semibold`}
+                      >
+                        {item.name}
+                      </span>
+                    </a>
+                  ))}
                 </div>
               </Tab.Panel>
             </Tab.Panels>
