ba62ea3f46670c25f6535655eea285848c748c80
Moussa Ibrahim <moussaebrahem32@gmail.com>
2023-08-17 15:08:58 +0300

    feat: final links page

--- pages/links.jsx
+++ pages/links.jsx
@@ -1,116 +1,197 @@
-import { Tab } from "@headlessui/react";
-import { Fragment, useMemo, useState } from "react";
-import { Open_Sans } from "@next/font/google";
-import { BiLogoFacebook, BiLogoTelegram } from "react-icons/bi";
-import { AiOutlineInstagram, AiOutlineTwitter } from "react-icons/ai";
 import Image from "next/image";
-import DeveLogo from "../public/assets/images/links-page-logo.svg";
-import useTranslation from "@/shared/Hooks/useTranslation";
-import copyArrowIcon from "../public/assets/images/copy-arrows.svg";
-import successCopyIcon from "../public/assets/images/success-copy.svg";
-import dynamic from "next/dynamic";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import { RxRocket, RxLink1 } from "react-icons/rx";
+import { GrFacebookOption, GrTwitter, GrStatusGood } from "react-icons/gr";
+import { FaTelegramPlane } from "react-icons/fa";
+import { BiLogoInstagramAlt, BiStore } from "react-icons/bi";
+import { AiOutlineSecurityScan } from "react-icons/ai";
+import { GiCrownCoin } from "react-icons/gi";
+import { useEffect, useState } from "react";
 import SideMenu from "@/shared/Components/SideMenu";
-import LinksSlider from "@/components/Links/LinksSlider";
-import PreSaleLogo from "./../public/logo.png";
+import FinanceImage from "@/images/financeImage.avif";
+import ScanToolImage from "@/images/scanTool.avif";
+import useTranslation from "@/shared/Hooks/useTranslation";
+import { LiaLayerGroupSolid } from "react-icons/lia";
+import { CgBriefcase } from "react-icons/cg";
+import copy from "copy-to-clipboard";
 
-const TabHeader = dynamic(() => import("@/components/Links/TabHeader"), {
-  ssr: false,
-});
-
-const openSans = Open_Sans({ subsets: ["latin"], weight: ["600", "700"] });
-
-const URLS = [
-  { icon: PreSaleLogo, url: "https://develocity.finance", text: "Presale Develocity" },
-  { url: "https://develocity.solutions", text: "develocity.solutions" },
-  { url: "https://develocity.finance", text: "develocity.finance" },
-  { url: "https://develocity.solutions", text: "develocity.solutions" },
-  { url: "https://develocity.finance", text: "develocity.finance" },
-  { url: "https://develocity.solutions", text: "develocity.solutions" },
+const links = [
+  {
+    Icon: LiaLayerGroupSolid,
+    image: FinanceImage,
+    url: "http://Develocity.group",
+    label: "Develocity Group",
+  },
+  {
+    Icon: GiCrownCoin,
+    image: FinanceImage,
+    url: "https://develocity.finance",
+    label: "Develocity PreSale",
+    isComing: false,
+  },
+  {
+    Icon: AiOutlineSecurityScan,
+    image: ScanToolImage,
+    url: "https://tool.develocity.finance",
+    label: "Develocity ScanTool",
+    isComing: false,
+  },
+  {
+    Icon: CgBriefcase,
+    image: FinanceImage,
+    url: "http://Develocity.solutions",
+    label: "Develocity Solutions",
+  },
+  {
+    Icon: BiStore,
+    image: FinanceImage,
+    url: "https://develocity.finance",
+    label: "Develocity Store",
+    isComing: true,
+  },
 ];
 
-const LinksPage = () => {
-  const [activeLink, setActiveLink] = useState({ linkCopiedIndex: null, copied: false });
+const LinkComponent = ({ Icon, label, isComing, url, onClick }) => {
+  const [isCopied, setIsCopied] = useState(false);
   const { t } = useTranslation("links");
-  const tabsHeader = useMemo(() => [{ title: t?.Develocitys }, { title: t?.socialMedia }], [t]);
-  const SOCIAL_MEDIA = useMemo(
-    () => [
-      { name: t?.facebook, icon: BiLogoFacebook, url: "https://facebook.com" },
-      { name: t?.instagram, icon: AiOutlineInstagram, url: "https://instagram.com" },
-      { name: t?.twitter, icon: AiOutlineTwitter, url: "https://x.com" },
-      { name: t?.telegram, icon: BiLogoTelegram, url: "https://telegram.com" },
-    ],
-    [t]
-  );
+
+  useEffect(() => {
+    if (isCopied) {
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+    }
+  }, [isCopied]);
+
+  const handleCopy = () => {
+    copy(url);
+    setIsCopied(true);
+  };
 
   return (
-    <section className="min-h-screen links-page-bg bg-cover">
-      <SideMenu />
-      <section className="container">
-        <Image src={DeveLogo} width={290} className="mx-auto block pt-32 xl:pt-60" alt="deve" />
-        <section className={`mx-auto pb-[115px] w-full ${openSans.className}`}>
-          <Tab.Group>
-            <Tab.List className="flex flex-col w-full items-start md:flex-row flex-nowrap space-y-6 md:space-y-0 md:space-x-[108px] xl:w-[880px] mx-auto rtl:space-x-reverse mt-16 xl:mt-24">
-              {tabsHeader.map((header, index) => (
-                <Tab key={`${header.title}${index}`} as={Fragment}>
-                  {({ selected }) => (
-                    <button
-                      className={`${
-                        selected ? "bg-indigo-500/50 border-opacity-70" : ""
-                      } outline-none flex items-center border-[3px] border-[#6A65E7] lg:border-x-0 lg:border-t-0 transition-all duration-150 justify-center text-lg lg:text-2xl w-full py-4 rounded text-white`}
-                    >
-                      {header.title}
-                    </button>
-                  )}
-                </Tab>
-              ))}
-            </Tab.List>
-            <Tab.Panels className="mt-20 xl:mt-24 border-1 w-full xl:w-[880px] mx-auto border-[#6A65E7] rounded-3xl pt-10 xl:pt-11 px-7 xl:px-[46px] pb-10 text-white">
-              <TabHeader />
-              <Tab.Panel>
-                <LinksSlider activeLink={activeLink} changeActiveLink={setActiveLink} urls={URLS} />
-                <div
-                  className={`flex justify-between items-center h-[44.41px] text-xl w-full xl:w-[386.54px] ${
-                    activeLink.copied ? `border-y-[1.41px] border-[#6A65E7] ltr:mr-auto rtl:ml-auto` : null
-                  } px-6`}
-                >
-                  {activeLink.copied && (
-                    <>
-                      <Image src={copyArrowIcon} alt="copy" />
-                      {t?.copied}
-                      <Image src={successCopyIcon} alt="success copy" />
-                    </>
-                  )}
-                </div>
-              </Tab.Panel>
-              <Tab.Panel>
-                <div className="flex flex-wrap items-center gap-y-10 gap-x-5 lg:gap-[75px] mt-14 lg:mt-[120.9px] mb-18 lg:mb-[84px] justify-center">
-                  {SOCIAL_MEDIA.map((item, index) => (
-                    <a
-                      key={`${item.name}${index}`}
-                      href={item.url}
-                      rel="noreferrer"
-                      target="_blank"
-                      className="grid grid-cols-1 place-content-center group"
-                    >
-                      <item.icon className="bg-[#6A65E7] text-5xl lg:text-7xl rounded-full mx-auto p-3 group-hover:bg-white group-hover:text-indigo-500" />
-                      <span
-                        className={`uppercase mt-4 lg:mt-10 ${openSans.className} text-base lg:text-xl font-semibold`}
-                      >
-                        {item.name}
-                      </span>
-                    </a>
-                  ))}
-                </div>
-              </Tab.Panel>
-            </Tab.Panels>
-          </Tab.Group>
-        </section>
-      </section>
-    </section>
+    <button
+      onClick={onClick}
+      className="flex items-center justify-start group gap-4 text-lg lg:text-xl ease-in-out hover:bg-black hover:text-white w-full hover:rounded py-2.5 hover:px-4 ltr:hover:pr-4 rtl:hover:pl-4 transition-all duration-300"
+    >
+      <Icon size={32} className="rounded-full" /> {label}
+      {isComing ? (
+        <span className="text-gray-700 text-xs ltr:ml-auto rtl:mr-auto group-hover:text-white">{t?.comingSoon}</span>
+      ) : (
+        <div className="flex items-center gap-4 ltr:ml-auto rtl:mr-auto">
+          <div className="bg-white p-1 rounded-full text-black" onClick={handleCopy}>
+            {isCopied ? <GrStatusGood /> : <RxLink1 />}
+          </div>
+          {!!url && (
+            <a href={url} target="_blank" rel="noreferrer" className="bg-white p-1 rounded-full text-black">
+              <RxRocket />
+            </a>
+          )}
+        </div>
+      )}
+    </button>
   );
 };
 
-export default LinksPage;
+const NewLinks = () => {
+  const [selectedLink, setSelectedLink] = useState(links[0]);
+  const { t } = useTranslation("links");
+
+  return (
+    <>
+      <section className="relative bg-indigo-500 pt-24 lg:pt-40">
+        <div className="absolute w-full h-full top-0">
+          <SideMenu />
+        </div>
+      </section>
+      <section className="container bg-white py-8 lg:py-20 flex flex-col gap-y-8 lg:flex-row items-start justify-center gap-x-12">
+        <section className="relative flex-shrink-0 w-full lg:w-[500px] xl:w-[600px] h-full">
+          <Image
+            src={selectedLink.image}
+            width={600}
+            height={600}
+            className="rounded-xl relative shadow-lg w-full h-[320px] sm:h-[450px] object-cover"
+            alt="Logo"
+            title="Logo"
+          />
+          <h1 className="absolute top-8 lg:top-10 text-3xl xl:text-5xl w-full text-center text-white font-bold">
+            {selectedLink.label}
+          </h1>
+          {!selectedLink.isComing && (
+            <a
+              href={selectedLink.url}
+              className="absolute flex items-center gap-2 justify-center bg-indigo-500 bottom-4 lg:bottom-8 transform left-1/2 -translate-x-1/2 text-white rounded py-3 px-7"
+            >
+              <RxRocket size={20} />
+              {t?.letsGo}
+            </a>
+          )}
+        </section>
+        <section className="w-full">
+          <h2 className="border-b-1 w-full mb-4 text-[#101828] font-PolySans font-bold text-2xl pb-2">
+            {t?.checkoutOurLinks}:
+          </h2>
+          <div className="flex flex-col gap-2 items-start mb-4">
+            {links.map((link, index) => (
+              <LinkComponent
+                onClick={() => setSelectedLink(link)}
+                key={`${link.label}${index}`}
+                Icon={link.Icon}
+                label={link.label}
+                url={link.url}
+                isComing={link.isComing}
+              />
+            ))}
+          </div>
+          <div className="mt-12">
+            <h2 className="border-b-1 w-full mb-4 text-[#101828] font-PolySans font-bold text-2xl pb-2">
+              {t?.socialMedia}:
+            </h2>
+            <div className="flex gap-4 flex-wrap items-start mb-4">
+              <a
+                href="https://facebook.com"
+                className="capitalize flex items-center transition-all ease-in-out duration-150 justify-start gap-4 text-sm w-[calc(50%_-_10px)] flex-initial hover:bg-indigo-500 hover:text-white group p-2 rounded"
+              >
+                <GrFacebookOption
+                  size={18}
+                  className="bg-black group-hover:bg-white group-hover:text-indigo-500 rounded-full p-2 box-content text-white"
+                />
+                {t?.facebook}
+              </a>
+              <a
+                href="https://facebook.com"
+                className="capitalize flex items-center transition-all ease-in-out duration-150 justify-start gap-4 text-sm w-[calc(50%_-_10px)] flex-initial hover:bg-indigo-500 hover:text-white group p-2 rounded"
+              >
+                <GrTwitter
+                  size={18}
+                  className="bg-black group-hover:bg-white group-hover:text-indigo-500 rounded-full p-2 box-content text-white"
+                />
+                {t?.x}
+              </a>
+              <a
+                href="https://facebook.com"
+                className="capitalize flex items-center transition-all ease-in-out duration-150 justify-start gap-4 text-sm w-[calc(50%_-_10px)] flex-initial hover:bg-indigo-500 hover:text-white group p-2 rounded"
+              >
+                <FaTelegramPlane
+                  size={18}
+                  className="bg-black group-hover:bg-white group-hover:text-indigo-500 rounded-full p-2 box-content text-white"
+                />
+                {t?.telegram}
+              </a>
+              <a
+                href="https://facebook.com"
+                className="capitalize flex items-center transition-all ease-in-out duration-150 justify-start gap-4 text-sm w-[calc(50%_-_10px)] flex-initial hover:bg-indigo-500 hover:text-white group p-2 rounded"
+              >
+                <BiLogoInstagramAlt
+                  size={18}
+                  className="bg-black group-hover:bg-white group-hover:text-indigo-500 rounded-full p-2 box-content text-white"
+                />
+                {t?.instagram}
+              </a>
+            </div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default NewLinks;
